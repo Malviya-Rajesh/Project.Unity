477e3cc9d3a96080639370760f3f7f31abe79644
Malviya-Rajesh <84065620+Malviya-Rajesh@users.noreply.github.com>
2021-05-30 17:02:10 +0530

    Adding new classes

---
+++ The Return/Assets/Scripts/Connection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Connection
+{
+    public string locationName;
+
+    public string description;
+
+    public Location location;
+
+    public bool connectionEnabled;
+}
---
+++ The Return/Assets/Scripts/Location.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location : MonoBehaviour
+{
+
+    public string locationName;
+
+    [TextArea]
+    public string description;
+
+    public Connection[] connection;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
